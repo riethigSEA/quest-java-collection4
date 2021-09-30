f883698133c73e0b02ae6cd0cad8989d743d681f
riethigSEA <marco-riethig_student2021@wilder.school>
2021-09-30 13:50:41 +0200

    Collection 04 done

--- Thanos.java
+++ Thanos.java
@@ -1,3 +1,6 @@
+
+import java.util.*;
+
 public class Thanos {
 
     public static void main(String[] args) {
@@ -15,16 +18,29 @@ public class Thanos {
         Flower begonia = new Flower("Begonia");
         Flower violet = new Flower("Violet");
         Flower rose = new Flower("Rose");
-
+        Map<Hero, Flower> party = new TreeMap<>(new Comparator<Hero>() {
+            @Override
+            public int compare(Hero o1, Hero o2) {
+                return o1.getName().compareToIgnoreCase(o2.getName());
+            }
+        });
         // TODO 1 : Create a TreeMap where :
         // Hulk, Thor and Scarlet Witch have a Rose
+        party.put(hulk,rose);
+        party.put(thor, rose);
+        party.put(scarletWitch, rose);
         // Vision has a Tulip
+        party.put(vision,tulip);
         // Captain America has a Lily
+        party.put(captainAmerica, lily);
         // Doctor Strange and Black Widow have a Violet
-
+        party.put(doctorStrange, violet);
+        party.put(blackWidow, violet);
         // TODO 2 : Print if `begonia` is contained in the TreeMap
-
+        System.out.println(party.containsValue(begonia));
         // TODO 3 : For each hero, alphabetically, print the corresponding flower
-
+        for (Flower flower : party.values()) {
+            System.out.println(flower.getName());
+        }
     }
 }
